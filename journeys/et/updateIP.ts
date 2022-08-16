608a56f1fe288931d4fe4da0df8b22cabd6833d6
Itay-Kirsh <Itayk747@gmail.com>
2022-08-16 15:00:53 +0100

    Journey that updates json.config.local.et_cos with WSL IP for Windows users.

---
+++ journeys/et/updateIP.ts
@@ -0,0 +1,25 @@
+import {Journey} from "types/journey"
+import {readFileSync, writeFileSync} from "fs";
+import {sep} from "path";
+import {execCommand} from "app/helpers";
+
+async function updateIP() {
+  const ip = (await execCommand("hostname -I | awk '{print $1}'")).stdout.trim();
+  updateRegion(process.env.ENGWALES_DEF_DIR, ip)
+  updateRegion(process.env.SCOTLAND_DEF_DIR, ip)
+}
+
+function updateRegion(regionDir: string, ip: string) {
+  const packageJsonPath = `${regionDir}${sep}package.json`;
+
+  const json = JSON.parse(readFileSync(packageJsonPath).toString())
+  // noinspection HttpUrlsUsage
+  json.config.local.et_cos = `http://${ip}:8081`
+  writeFileSync(packageJsonPath, JSON.stringify(json, null, 2))
+}
+
+export default {
+  group: 'et-docker',
+  text: 'Update IP address in package.json of config repos',
+  fn: updateIP
+} as Journey
