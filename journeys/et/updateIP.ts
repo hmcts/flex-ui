382abb5b59a4d23acb6eceb2c4c7fa07ea2f63d0
Itay-Kirsh <Itayk747@gmail.com>
2022-08-16 15:21:05 +0100

    Fix styling

--- journeys/et/updateIP.ts
+++ journeys/et/updateIP.ts
@@ -1,16 +1,16 @@
-import {Journey} from "types/journey"
-import {readFileSync, writeFileSync} from "fs";
-import {sep} from "path";
-import {execCommand} from "app/helpers";
+import { Journey } from "types/journey"
+import { readFileSync, writeFileSync } from "fs"
+import { sep } from "path"
+import { execCommand } from "app/helpers"
 
 async function updateIP() {
-  const ip = (await execCommand("hostname -I | awk '{print $1}'")).stdout.trim();
+  const ip = (await execCommand("hostname -I | awk '{print $1}'")).stdout.trim()
   updateRegion(process.env.ENGWALES_DEF_DIR, ip)
   updateRegion(process.env.SCOTLAND_DEF_DIR, ip)
 }
 
 function updateRegion(regionDir: string, ip: string) {
-  const packageJsonPath = `${regionDir}${sep}package.json`;
+  const packageJsonPath = `${regionDir}${sep}package.json`
 
   const json = JSON.parse(readFileSync(packageJsonPath).toString())
   // noinspection HttpUrlsUsage
