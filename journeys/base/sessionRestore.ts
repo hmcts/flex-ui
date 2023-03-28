3cf8ba1025e641127e244817ceeb0b5baa0f42b5
Jack Reeve <jack@reevies.tech>
2023-03-28 17:38:03 +0100

    PR comments

--- journeys/base/sessionRestore.ts
+++ journeys/base/sessionRestore.ts
@@ -1,7 +1,9 @@
-import { findPreviousSessions, restorePreviousSession } from 'app/session'
+import { findPreviousSessions, restorePreviousSession, session } from 'app/session'
 import { Journey } from 'types/journey'
 import { askAutoComplete } from 'app/questions'
-import { CANCEL } from 'app/constants'
+import { CANCEL, COMPOUND_KEYS } from 'app/constants'
+import { sheets } from 'app/configs'
+import { upsertFields } from 'app/helpers'
 
 const QUESTION_PREVIOUS_SESSION = 'Select a previous session'
 
@@ -34,9 +36,17 @@ export async function restoreSession() {
   restorePreviousSession(answers.name)
 }
 
+export async function restoreSessionJourney() {
+  await restoreSession()
+
+  for (const sheetName in sheets) {
+    upsertFields(sheets[sheetName], session.added[sheetName], COMPOUND_KEYS[sheetName])
+  }
+}
+
 export default {
   group: 'session',
   text: 'Restore a previous session',
-  fn: restoreSession,
+  fn: restoreSessionJourney,
   alias: 'SessionRestore'
 } as Journey
